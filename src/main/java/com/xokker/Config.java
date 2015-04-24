0dbdc6b07892039519fbfee3e8bc2e592c9b85ca
Ernest Sadykov <ernsad@gmail.com>
2015-04-24 19:32:06 +0300

    configuration file

---
+++ src/main/java/com/xokker/Config.java
@@ -0,0 +1,30 @@
+package com.xokker;
+
+import org.apache.commons.lang3.BooleanUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Ernest Sadykov
+ * @since 24.04.2015
+ */
+public class Config {
+
+    private static boolean quiteMode;
+
+    static {
+        Properties config = new Properties();
+        try {
+            config.load(Config.class.getResourceAsStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        quiteMode = BooleanUtils.toBoolean(config.getProperty("quite_mode", "false"));
+    }
+
+    public static boolean isQuiteMode() {
+        return quiteMode;
+    }
+
+}
--- src/main/java/com/xokker/util/MatrixPrinter.java
+++ src/main/java/com/xokker/util/MatrixPrinter.java
@@ -1,5 +1,6 @@
 package com.xokker.util;
 
+import com.xokker.Config;
 import org.apache.commons.math3.linear.RealMatrix;
 
 import java.io.PrintStream;
@@ -16,6 +17,9 @@ public class MatrixPrinter {
     }
 
     public static void println(RealMatrix matrix, PrintStream printStream) {
+        if (Config.isQuiteMode()) {
+            return;
+        }
         for (int row = 0; row < matrix.getRowDimension(); row++) {
             printStream.println(Arrays.toString(matrix.getRow(row)));
         }
