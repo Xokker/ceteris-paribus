d3b7f484bacb85e3fd1f30fcdf0c7e85b936a185
Ernest Sadykov <ernsad@gmail.com>
2015-05-27 00:03:24 +0300

    changed implementation of CPfs

--- src/main/java/com/xokker/predictor/impl/CeterisParibusPredictor.java
+++ src/main/java/com/xokker/predictor/impl/CeterisParibusPredictor.java
@@ -8,9 +8,7 @@ import com.xokker.predictor.PreferencePredictor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 import static com.google.common.collect.Sets.*;
 import static java.util.Collections.singleton;
@@ -56,75 +54,15 @@ public class CeterisParibusPredictor<A extends Attribute> implements PreferenceP
                     );
                     if (checkPreference(d, f, e)) {
                         logger.trace("{} <{}= {}    for {} and {}", d, f, e, a, b);
-                        result.add(new Support(g, h));
+//                        result.add(new Support(g, h));
+                        return singleton(new Support(g, h));
                     }
                 }
             }
         }
 
-        return result;
-    }
-
-    /**
-     * TODO: optimize
-     */
-    public Set<Support> fixed(Set<A> a, Set<A> b, CeterisParibusPreference<A> fixed) {
-        Set<Identifiable> allObjects = context.getAllObjects();
-        Set<A> allAttributes = context.getAllAttributes();
-        Set<Support> result = new HashSet<>();
-        for (Identifiable g : allObjects) {
-            Set<A> d = context.getObjectIntent(g);
-
-//            if (d.equals(fixed.getD())) {
-            if (intersection(d, fixed.getD()).size() == fixed.getD().size()) {
-                for (Identifiable h : difference(allObjects, singleton(g))) {
-                    if (context.leq(g, h) == PrefState.Leq) {
-                        Set<A> hIntent = context.getObjectIntent(h);
-                        Set<A> gIntent = context.getObjectIntent(g);
-                        Set<A> e = hIntent;
-                        Set<A> f = intersection(
-                                difference(allAttributes, symmetricDifference(a, b)),
-                                difference(allAttributes, symmetricDifference(gIntent, hIntent))
-                        );
-//                        if (f.equals(fixed.getF()) && e.equals(fixed.getE())) {
-                        if (f.equals(fixed.getF()) && intersection(e, fixed.getE()).size() == fixed.getE().size()) {
-                            if (checkPreference(d, f, e)) {
-                                logger.trace("{} <{}= {}    for {} and {}", d, f, e, a, b);
-                                result.add(new Support(g, h));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private Set<CeterisParibusPreference<A>> findAll(Set<A> a, Set<A> b) {
-        Set<Identifiable> allObjects = context.getAllObjects();
-        Set<A> allAttributes = context.getAllAttributes();
-        Set<CeterisParibusPreference<A>> result = new HashSet<>();
-        for (Identifiable g : allObjects) {
-            Set<A> d = intersection(a, context.getObjectIntent(g));
-            for (Identifiable h : difference(allObjects, singleton(g))) {
-                if (context.leq(g, h) == PrefState.Leq) {
-                    Set<A> hIntent = context.getObjectIntent(h);
-                    Set<A> gIntent = context.getObjectIntent(g);
-                    Set<A> e = intersection(b, hIntent);
-                    Set<A> f = intersection(
-                            difference(allAttributes, symmetricDifference(a, b)),
-                            difference(allAttributes, symmetricDifference(gIntent, hIntent))
-                    );
-                    if (checkPreference(d, f, e)) {
-                        logger.trace("{} <{}= {}    for {} and {}", d, f, e, a, b);
-                        result.add(new CeterisParibusPreference<>(d, f, e));
-                    }
-                }
-            }
-        }
-
-        return result;
+//        return result;
+        return Collections.emptySet();
     }
 
     /**
@@ -132,41 +70,11 @@ public class CeterisParibusPredictor<A extends Attribute> implements PreferenceP
      */
     @Override
     public int predict(Set<A> first, Set<A> second) {
-        Set<CeterisParibusPreference<A>> allLeftRight = findAll(first, second);
-        Set<CeterisParibusPreference<A>> allRightLeft = findAll(second, first);
+        Map<Integer, CeterisParibusPreference<A>> lr = predictPreferenceCounted(first, second);
+        Map<Integer, CeterisParibusPreference<A>> rl = predictPreferenceCounted(second, first);
 
-        int maxLeftRight = 0;
-        int maxRightLeft = 0;
-
-        Set<Support> fixedlr_max = null;
-        Set<Support> fixedrl_max = null;
-
-        Iterator<CeterisParibusPreference<A>> iterator = allRightLeft.iterator();
-        for (CeterisParibusPreference<A> fixed : allLeftRight) {
-            Set<Support> fixedlr = fixed(first, second, fixed);
-            int leftRight = fixedlr.size();
-            if (leftRight > maxLeftRight) {
-                maxLeftRight = leftRight;
-                fixedlr_max = fixedlr;
-                while (maxLeftRight >= maxRightLeft && iterator.hasNext()) {
-                    CeterisParibusPreference<A> next = iterator.next();
-                    Set<Support> fixedrl = fixed(second, first, next);
-                    int rightLeft = fixedrl.size();
-                    if (rightLeft > maxRightLeft) {
-                        maxRightLeft = rightLeft;
-                        fixedrl_max = fixedrl;
-                    }
-                }
-            }
-        }
-
-        if (maxLeftRight == maxRightLeft && maxLeftRight == 0) {
-            Set<Support> supportLR = predictPreference(first, second);
-            Set<Support> supportRL = predictPreference(second, first);
-            return supportRL.size() - supportLR.size();
-        }
-
-        return maxRightLeft - maxLeftRight;
+        return rl.keySet().stream().max(Comparator.<Integer>naturalOrder()).orElseGet(() -> 0) -
+                lr.keySet().stream().max(Comparator.<Integer>naturalOrder()).orElseGet(() -> 0);
     }
 
     /**
@@ -188,4 +96,53 @@ public class CeterisParibusPredictor<A extends Attribute> implements PreferenceP
         return true;
     }
 
+    public Map<Integer, CeterisParibusPreference<A>> predictPreferenceCounted(Set<A> a, Set<A> b) {
+        Set<Identifiable> allObjects = context.getAllObjects();
+        Set<A> allAttributes = context.getAllAttributes();
+
+        Map<Integer, CeterisParibusPreference<A>> result = new HashMap<>();
+        for (Identifiable g : allObjects) {
+            Set<A> d = intersection(a, context.getObjectIntent(g));
+            for (Identifiable h : difference(allObjects, singleton(g))) {
+                if (context.leq(g, h) == PrefState.Leq) {
+                    Set<A> hIntent = context.getObjectIntent(h);
+                    Set<A> gIntent = context.getObjectIntent(g);
+                    Set<A> e = intersection(b, hIntent);
+                    Set<A> f = intersection(
+                            difference(allAttributes, symmetricDifference(a, b)),
+                            difference(allAttributes, symmetricDifference(gIntent, hIntent))
+                    );
+                    int c = checkPreferenceCounted(d, f, e);
+                    if (c > 0) {
+                        logger.trace("{} <{}= {}    for {} and {}", d, f, e, a, b);
+                        result.put(c, new CeterisParibusPreference<A>(d, f, e));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    int checkPreferenceCounted(Set<A> d, Set<A> f, Set<A> e) {
+        int c = 0;
+        Set<Identifiable> x = context.getAttributeExtent(d);
+        Set<Identifiable> y = context.getAttributeExtent(e);
+        for (Identifiable g : x) {
+            for (Identifiable h : y) {
+                Set<A> gIntent = context.getObjectIntent(g);
+                Set<A> hIntent = context.getObjectIntent(h);
+                if (Objects.equals(intersection(gIntent, f), (intersection(hIntent, f)))) {
+                    if (context.leq(g, h) != PrefState.Leq) {
+                        return 0;
+                    } else {
+                        c++;
+                    }
+                }
+            }
+        }
+
+        return c;
+    }
 }
